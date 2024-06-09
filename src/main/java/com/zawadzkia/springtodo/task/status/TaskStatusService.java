5a3a20d958198d57211bd6d461fb2907a128d5be
kierk <kierklo.piotr@gmail.com>
2024-06-09 21:16:28 +0200

    dodano tworzenia i usuwania statusu

--- src/main/java/com/zawadzkia/springtodo/task/status/TaskStatusService.java
+++ src/main/java/com/zawadzkia/springtodo/task/status/TaskStatusService.java
@@ -1,9 +1,7 @@
 package com.zawadzkia.springtodo.task.status;
 
 import com.zawadzkia.springtodo.auth.AppUserDetails;
-import com.zawadzkia.springtodo.task.category.TaskCategoryModel;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.config.Task;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -16,25 +14,36 @@ public class TaskStatusService {
 
     private final TaskStatusRepository taskStatusRepository;
 
-    public TaskStatusModel getTaskStatusModelByNazwaAndUser(String nazwa)
-    {
+    public TaskStatusModel getTaskStatusModelByNazwaAndUser(String nazwa) {
         TaskStatusModel result = new TaskStatusModel();
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if(principal instanceof AppUserDetails userDetails) {
-            TaskStatusModel byOwner = taskStatusRepository.findByNameAndOwner(nazwa,userDetails.getUser());
+        if (principal instanceof AppUserDetails userDetails) {
+            TaskStatusModel byOwner = taskStatusRepository.findByNameAndOwner(nazwa, userDetails.getUser());
             result = byOwner;
         }
         return result;
     }
-    public List<TaskStatusDTO> getUserTaskStatusList() {
-        ArrayList<TaskStatusDTO> result = new ArrayList<>();
+
+    public List<TaskStatusDTO> getStatuses() { // Renamed method
+        List<TaskStatusDTO> result = new ArrayList<>();
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if(principal instanceof AppUserDetails userDetails) {
+        if (principal instanceof AppUserDetails userDetails) {
             List<TaskStatusModel> allByOwner = taskStatusRepository.findAllByOwner(userDetails.getUser());
-            allByOwner.forEach(status -> result.add(new TaskStatusDTO(status.getId(), status.getName(),
-                    status.getDisplayName())));
+            allByOwner.forEach(status -> result.add(new TaskStatusDTO(status.getId(), status.getName(), status.getDisplayName())));
         }
         return result;
     }
 
+    public void addStatusWithUser(TaskStatusModel statusModel) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof AppUserDetails userDetails) {
+            statusModel.setOwner(userDetails.getUser());
+            taskStatusRepository.save(statusModel);
+        }
+    }
+
+    public void delete(Long id) {
+        TaskStatusModel statusModel = taskStatusRepository.getReferenceById(id);
+        taskStatusRepository.delete(statusModel);
+    }
 }
